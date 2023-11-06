044459cce0baf8da2ddc6e20ef095886bd3c2eb4
nancy <gautamnancy324@gmail.com>
2023-11-06 21:51:06 +0530

    Created generated reset password token method

--- Models/user.model.js
+++ Models/user.model.js
@@ -70,7 +70,22 @@ userSchema.methods = {
       );
     },
 
-    
+    generatePasswordResetToken: async function () {
+      // creating a random token using node's built-in crypto module
+      const resetToken = crypto.randomBytes(20).toString('hex');
+  
+      // Again using crypto module to hash the generated resetToken with sha256 algorithm and storing it in database
+      this.forgotPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+  
+      // Adding forgot password expiry to 15 minutes
+      this.forgotPasswordExpiry = Date.now() + 15 * 60 * 1000;
+  
+      return resetToken;
+    },
+
     
   };
 
