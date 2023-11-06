ccab34fea1002d7aac914b6b22697e53e1457978
nancy <gautamnancy324@gmail.com>
2023-11-06 21:49:38 +0530

    Forgot password controller created

--- Models/user.model.js
+++ Models/user.model.js
@@ -1,6 +1,7 @@
 import { Schema , model } from "mongoose";
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import crypto from 'crypto'
 
 const userSchema = new Schema({
         fullName:{
@@ -68,6 +69,8 @@ userSchema.methods = {
         }
       );
     },
+
+    
     
   };
 
--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, logout, register } from "../Controllers/user.controller.js";
+import { resetPassword, forgotPassword, login, logout, register } from "../Controllers/user.controller.js";
 import upload from "../Middlewares/multer.middleware.js";
 
 const router = Router();
@@ -8,5 +8,6 @@ router.post('/register',upload.single("avatar"), register);
 // router.get('/me', getProfile);
 router.post('/login',login);
 router.post('/logout', logout);
-
+router.post("/reset", forgotPassword)
+router.post("/reset/:resetToken", resetPassword);
 export default router;
